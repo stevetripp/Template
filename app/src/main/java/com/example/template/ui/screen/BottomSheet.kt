ac17500544941691b31019c9492be3f7d4d8855d
Steve Tripp <stripp@churchofjesuschrist.org>
2021-10-01 14:25:15 -0600

    Update bottom sheet example

--- app/src/main/java/com/example/template/ui/screen/BottomSheet.kt
+++ app/src/main/java/com/example/template/ui/screen/BottomSheet.kt
@@ -1,28 +1,21 @@
 package com.example.template.ui.screen
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.Button
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FabPosition
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -33,7 +26,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.template.AppBar
 import com.example.template.Nav
@@ -43,95 +35,57 @@ import kotlinx.coroutines.launch
 @Composable
 fun BottomSheet(nav: Nav, onBack: () -> Unit) {
     BackHandler(onBack = onBack)
+    val scope = rememberCoroutineScope()
+    val scaffoldState = rememberBottomSheetScaffoldState()
     var showSnackbar by remember { mutableStateOf(false) }
-    Scaffold(topBar = { AppBar(nav, onBack) }) {
-        val scope = rememberCoroutineScope()
-        val scaffoldState = rememberBottomSheetScaffoldState()
-        Box(
-            modifier = Modifier
-                .background(Color.Blue)
-                .fillMaxSize()
-        )
-        BottomSheetScaffold(
-            sheetContent = {
-                Box(
-                    Modifier
-                        .fillMaxWidth()
-                        .height(128.dp),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text("Swipe up to expand sheet")
-                }
-                Column(
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(64.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text("Sheet content")
-                    Spacer(Modifier.height(20.dp))
-                    Button(
-                        onClick = {
-                            scope.launch { scaffoldState.bottomSheetState.collapse() }
-                        }
-                    ) {
-                        Text("Click to collapse sheet")
+    BottomSheetScaffold(
+        topBar = { AppBar(nav, onBack) },
+        sheetContent = {
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(128.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Swipe up to expand sheet")
+            }
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(64.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Sheet content")
+                Spacer(Modifier.height(20.dp))
+                Button(
+                    onClick = {
+                        scope.launch { scaffoldState.bottomSheetState.collapse() }
                     }
-                }
-            },
-            scaffoldState = scaffoldState,
-//            topBar ={ AppBar(nav, onBack) },
-            topBar = {
-                TopAppBar(
-                    title = { Text("Bottom sheet scaffold") },
-                    navigationIcon = {
-                        IconButton(onClick = { scope.launch { scaffoldState.drawerState.open() } }) {
-                            Icon(Icons.Default.Menu, contentDescription = "Localized description")
-                        }
-                    }
-                )
-            },
-            floatingActionButton = {
-                var clickCount by remember { mutableStateOf(0) }
-                FloatingActionButton(
-                    onClick = { showSnackbar = true }
                 ) {
-                    Icon(Icons.Default.Favorite, contentDescription = "Localized description")
-                }
-            },
-            floatingActionButtonPosition = FabPosition.End,
-//            sheetPeekHeight = 128.dp,
-//            drawerContent = {
-//                Column(
-//                    Modifier
-//                        .fillMaxWidth()
-//                        .padding(16.dp),
-//                    horizontalAlignment = Alignment.CenterHorizontally
-//                ) {
-//                    Text("Drawer content")
-//                    Spacer(Modifier.height(20.dp))
-//                    Button(onClick = { scope.launch { scaffoldState.drawerState.close() } }) {
-//                        Text("Click to close drawer")
-//                    }
-//                }
-//            }
-        ) { innerPadding ->
-            LazyColumn(contentPadding = innerPadding) {
-                items(100) {
-                    Box(
-                        Modifier
-                            .fillMaxWidth()
-                            .height(50.dp)
-                            .background(Color.Blue)
-                    )
+                    Text("Click to collapse sheet")
                 }
             }
+        },
+        scaffoldState = scaffoldState,
+        floatingActionButton = {
+            var clickCount by remember { mutableStateOf(0) }
+            FloatingActionButton(
+                onClick = { showSnackbar = true }
+            ) {
+                Icon(Icons.Default.Favorite, contentDescription = "Localized description")
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        sheetPeekHeight = 0.dp,
+    ) {
+        Button(onClick = { scope.launch { scaffoldState.bottomSheetState.expand() } }) {
+            Text("Show bottom sheet")
         }
-        LaunchedEffect(key1 = showSnackbar) {
-            if (showSnackbar) {
-                scaffoldState.snackbarHostState.showSnackbar("Message")
-                showSnackbar = false
-            }
+    }
+    LaunchedEffect(key1 = showSnackbar) {
+        if (showSnackbar) {
+            scaffoldState.snackbarHostState.showSnackbar("Floating action button clicked.")
+            showSnackbar = false
         }
     }
 }
