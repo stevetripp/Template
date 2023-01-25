63d50eecaeebe8902f4868f6543b5061cfe9aaa9
Steve Tripp <stripp@churchofjesuschrist.org>
2023-01-25 12:01:26 -0700

    Gradle 7.6, AGP 7.4.0, and Kotlin 1.8.0

--- app/src/main/java/com/example/template/ux/modalbottomsheet/ModalBottomSheetScreen.kt
+++ app/src/main/java/com/example/template/ux/modalbottomsheet/ModalBottomSheetScreen.kt
@@ -1,5 +1,6 @@
 package com.example.template.ux.modalbottomsheet
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,12 +18,18 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.template.ui.PreviewDefault
 import com.example.template.ui.composable.AppTopAppBar
+import com.example.template.ui.theme.AppTheme
 import com.example.template.ux.main.Screen
 import kotlinx.coroutines.launch
 
@@ -32,7 +39,8 @@ fun ModalBottomSheetScreen(navController: NavController) {
 }
 
 @Composable
-private fun ModalBottomSheetContent(onBack: () -> Unit) {
+private fun ModalBottomSheetContent(onBack: () -> Unit = {}) {
+    var text by remember { mutableStateOf("") }
     val state = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
     val scope = rememberCoroutineScope()
     ModalBottomSheetLayout(
@@ -41,8 +49,13 @@ private fun ModalBottomSheetContent(onBack: () -> Unit) {
             // sheetContent MUST have at least one composable or an exception is thrown
             LazyColumn {
                 items(50) {
+                    val listItemText = "Item $it"
                     ListItem(
-                        text = { Text("Item $it") },
+                        modifier = Modifier.clickable {
+                            text = listItemText
+                            scope.launch { state.hide() }
+                        },
+                        text = { Text(listItemText) },
                         icon = {
                             Icon(
                                 Icons.Default.Favorite,
@@ -58,10 +71,12 @@ private fun ModalBottomSheetContent(onBack: () -> Unit) {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
+                    .padding(paddingValues)
                     .padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text("Rest of the UI")
+                Text(text)
                 Spacer(Modifier.height(20.dp))
                 Button(onClick = { scope.launch { state.show() } }) {
                     Text("Click to show sheet")
@@ -69,4 +84,10 @@ private fun ModalBottomSheetContent(onBack: () -> Unit) {
             }
         }
     }
+}
+
+@PreviewDefault
+@Composable
+private fun ModalBottomSheetContentPreview() {
+    AppTheme { ModalBottomSheetContent() }
 }
