eba04383e369dce7337115d893e0c79b25ed07ab
Steve Tripp <stripp@churchofjesuschrist.org>
2023-06-28 15:50:40 -0600

    Update ModalBottomSheet to show different sized lists.

---
+++ app/src/main/java/com/example/template/ui/widget/RadioButtonAndText.kt
@@ -0,0 +1,50 @@
+package com.example.template.ui.widget
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
+import com.example.template.ui.PreviewDefault
+import com.example.template.ui.theme.AppTheme
+
+@Composable
+fun RadioButtonAndText(text: String, selected: Boolean, onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .height(45.dp)
+            .selectable(
+                selected = selected,
+                onClick = onClick,
+                role = Role.RadioButton
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        RadioButton(selected = selected, onClick = null)
+        Text(text = text)
+    }
+}
+
+@PreviewDefault
+@Composable
+private fun RadioButtonAndTextPreview() {
+    AppTheme {
+        Surface {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                RadioButtonAndText(text = "Radio Button 1", selected = true, onClick = {})
+                RadioButtonAndText(text = "Radio Button 2", selected = false, onClick = {})
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/template/ux/modalbottomsheet/ModalBottomSheetScreen.kt
+++ app/src/main/java/com/example/template/ux/modalbottomsheet/ModalBottomSheetScreen.kt
@@ -1,17 +1,13 @@
 package com.example.template.ux.modalbottomsheet
 
-import android.util.Log
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material3.Button
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
@@ -24,13 +20,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.template.ui.PreviewDefault
 import com.example.template.ui.composable.AppTopAppBar
 import com.example.template.ui.theme.AppTheme
+import com.example.template.ui.widget.RadioButtonAndText
 import com.example.template.ux.main.Screen
 import kotlinx.coroutines.launch
 
@@ -45,20 +40,24 @@ private fun ModalBottomSheetContent(onBack: () -> Unit = {}) {
     val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = false)
     var text by remember { mutableStateOf("") }
     val scope = rememberCoroutineScope()
+    var radioButtonsData by remember {
+        mutableStateOf(listOf("One Item" to 1, "Ten Items" to 10, "One Hundred Items" to 100).map { RadioButtonData(it.first, it.second, it.first == "One Item") })
+    }
 
     if (showBottomSheet) {
+        val selectedRbData = radioButtonsData.find { it.isSelected } ?: radioButtonsData[0]
         ModalBottomSheet(
             sheetState = bottomSheetState,
             onDismissRequest = { showBottomSheet = false }) {
             // sheetContent MUST have at least one composable or an exception is thrown
             LazyColumn {
-                items(50) {
+                items(selectedRbData.itemCount) {
                     val listItemText = "Item $it"
                     ListItem(
                         modifier = Modifier.clickable {
                             text = listItemText
                             scope.launch {
-                                bottomSheetState.hide()
+                                bottomSheetState.hide() // Added for animation
                                 showBottomSheet = false
                             }
                         },
@@ -75,21 +74,20 @@ private fun ModalBottomSheetContent(onBack: () -> Unit = {}) {
         }
     }
     Scaffold(topBar = { AppTopAppBar(title = Screen.MODAL_BOTTOM_SHEET.title, onBack = onBack) }) { paddingValues ->
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues)
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(paddingValues),
         ) {
-            Text("Rest of the UI")
-            Text(text)
-            Spacer(Modifier.height(20.dp))
-            Button(onClick = {
-                Log.i("SMT", "clicked")
-                showBottomSheet = true
-            }) {
-                Text("Click to show sheet")
+            items(radioButtonsData) { rbData ->
+                RadioButtonAndText(
+                    text = rbData.text,
+                    selected = rbData.isSelected,
+                    onClick = {
+                        showBottomSheet = true
+                        radioButtonsData = radioButtonsData.map { it.copy(isSelected = it.text == rbData.text) }
+                    }
+                )
             }
         }
     }
@@ -100,3 +98,5 @@ private fun ModalBottomSheetContent(onBack: () -> Unit = {}) {
 private fun ModalBottomSheetContentPreview() {
     AppTheme { ModalBottomSheetContent() }
 }
+
+private data class RadioButtonData(val text: String, val itemCount: Int, val isSelected: Boolean = false)
