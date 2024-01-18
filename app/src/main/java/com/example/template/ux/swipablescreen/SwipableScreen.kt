d4fbbdf266a4cb0d905c264ca45bb5dd7f4290bd
Steve Tripp <steven.m.tripp@gmail.com>
2024-01-18 12:56:07 -0700

    Updates

--- app/src/main/java/com/example/template/ux/swipablescreen/SwipableScreen.kt
+++ app/src/main/java/com/example/template/ux/swipablescreen/SwipableScreen.kt
@@ -7,15 +7,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SwipeToDismissBox
-import androidx.compose.material3.SwipeToDismissValue
+import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberSwipeToDismissState
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -44,11 +44,11 @@ fun SwipableContent(onBack: () -> Unit = {}) {
     Scaffold(topBar = { AppTopAppBar(title = Screen.SWIPABLE.title, onBack = onBack) }) { paddingValues ->
         LazyColumn(modifier = Modifier.padding(paddingValues)) {
             items(items, key = { it.id }) { item ->
-                val dismissState = rememberSwipeToDismissState(
+                val dismissState = rememberSwipeToDismissBoxState(
                     confirmValueChange = { dismissValue ->
                         Log.i("SMT", "DismissValue: $dismissValue")
                         when (dismissValue) {
-                            SwipeToDismissValue.StartToEnd -> {
+                            SwipeToDismissBoxValue.StartToEnd -> {
                                 scope.launch {
                                     delay(500)
                                     items = items.toMutableList().apply { remove(item) }
