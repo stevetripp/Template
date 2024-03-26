d0b757e18005292925b122f4df1798640ffd2624
Steve Tripp <steven.m.tripp@gmail.com>
2024-03-26 14:25:37 -0600

    Fix carousel

--- app/src/main/java/com/example/template/ui/widget/Carousel.kt
+++ app/src/main/java/com/example/template/ui/widget/Carousel.kt
@@ -1,6 +1,6 @@
 package com.example.template.ui.widget
 
-import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerScope
@@ -9,14 +9,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+private const val MAX_CAROUSEL_ITEMS = 30000
+
 @Composable
 fun Carousel(itemCount: Int, modifier: Modifier = Modifier, content: @Composable PagerScope.(itemIndex: Int) -> Unit) {
-    BoxWithConstraints(modifier = modifier) {
+    Box(modifier = modifier) {
         val contentPadding = if (itemCount > 1) 24.dp else 8.dp
 
         val pagerState = rememberPagerState(
-            initialPage = (Int.MAX_VALUE / 2) - ((Int.MAX_VALUE / 2) % itemCount),
-            pageCount = { if (itemCount == 1) 1 else Int.MAX_VALUE }
+            initialPage = (MAX_CAROUSEL_ITEMS / 2) - ((MAX_CAROUSEL_ITEMS / 2) % itemCount),
+            pageCount = { if (itemCount == 1) 1 else MAX_CAROUSEL_ITEMS }
         )
 
         HorizontalPager(
