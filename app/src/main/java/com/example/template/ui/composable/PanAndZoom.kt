870d29774d1185463c729ee7f50a89f0d6b1f09a
Steve Tripp <steven.m.tripp@gmail.com>
2024-04-16 12:27:09 -0600

    Fix PanAndZoom

--- app/src/main/java/com/example/template/ui/composable/PanAndZoom.kt
+++ app/src/main/java/com/example/template/ui/composable/PanAndZoom.kt
@@ -1,7 +1,5 @@
 package com.example.template.ui.composable
 
-import androidx.compose.foundation.gestures.rememberTransformableState
-import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.BoxWithConstraintsScope
 import androidx.compose.runtime.Composable
@@ -11,29 +9,49 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.pointerInput
 
 /**
  * [Composable] that provides panning and zoom functionality for [content]
  */
 @Composable
-fun PanAndZoom(modifier: Modifier = Modifier, scalingLimit: ClosedRange<Float> = (.5F..4F), content: @Composable (BoxWithConstraintsScope.(scale: Float) -> Unit)) {
+fun PanAndZoom(
+    modifier: Modifier = Modifier,
+    scalingLimit: ClosedRange<Float> = (.5F..4F),
+    content: @Composable (BoxWithConstraintsScope.(scale: Float) -> Unit)
+) {
 
     var scale by remember { mutableStateOf(1f) }
     var offset by remember { mutableStateOf(Offset.Zero) }
-    val state = rememberTransformableState { zoomChange, offsetChange, _ ->
-        scale = (scale * zoomChange).coerceIn(scalingLimit)
-        offset += offsetChange
-    }
     BoxWithConstraints(
         modifier = modifier
+            .pointerInput(Unit) {
+                detectTransformGestures(
+                    consume = false,
+                    pass = PointerEventPass.Initial,
+                    onGesture = { centroid, pan, gestureZoom, _, _, changes ->
+                        val oldScale = scale
+                        val newScale = (scale * gestureZoom).coerceIn(scalingLimit)
+                        val isTwoFingerGesture = changes.size >= 2
+                        if (isTwoFingerGesture) {
+                            offset = (offset + centroid / oldScale) - (centroid / newScale + pan / oldScale)
+                            scale = newScale
+                            //Consume here so lifting one finger while zooming/panning does NOT count as another gesture
+                            changes.forEach { it.consume() }
+                        }
+                    },
+                )
+            }
             .graphicsLayer(
                 scaleX = scale,
                 scaleY = scale,
-                translationX = offset.x * scale,
-                translationY = offset.y * scale
+                translationX = -offset.x * scale,
+                translationY = -offset.y * scale,
+                transformOrigin = TransformOrigin(0f, 0f)
             )
-            .transformable(state = state)
     ) {
         content(scale)
     }
