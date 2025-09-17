707fd4f09b2019aef96011bc10bc94c5600653c0
Steve Tripp <steven.m.tripp@gmail.com>
2025-09-17 15:30:44 -0600

    Refactor `VERSION_CODE` property in `AppInfo`
    This commit renames the `VERSION_CODE` property to `CODE` within the `AppInfo.Version` object for conciseness. The `NAME` property was also updated to reflect this change.

--- buildSrc/src/main/kotlin/AppInfo.kt
+++ buildSrc/src/main/kotlin/AppInfo.kt
@@ -7,9 +7,9 @@ object AppInfo {
     object Version {
         const val MIN = 1810 // Specify the Min version code here and in the .github/workflows/release.yml MIN_VERSION_CODE variable
         private const val SEMANTIC_VERSION = "1.0.2"
-        val VERSION_CODE = System.getenv("VERSION_CODE")?.toIntOrNull() ?: MIN
+        val CODE = System.getenv("VERSION_CODE")?.toIntOrNull() ?: MIN
         val RUN_NUMBER = System.getenv("RUN_NUMBER").orEmpty()
-        val NAME = """$SEMANTIC_VERSION-($VERSION_CODE.$RUN_NUMBER)"""
+        val NAME = """$SEMANTIC_VERSION-($CODE.$RUN_NUMBER)"""
     }
 
     object AndroidSdk {
