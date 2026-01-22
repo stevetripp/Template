cce3c4870206ac007cb939c9e25d7628b0ae7dc5
Steve Tripp <steven.m.tripp@gmail.com>
2026-01-22 15:29:44 -0700

    koin: migrate from Dagger Hilt to Koin for dependency injection
    Refactor application setup to utilize Koin for dependency injection
    instead of Dagger Hilt. This change includes updates to the
    AndroidManifest and App class to support Koin's initialization
    process with AndroidX Startup.

--- app/src/main/java/com/example/template/App.kt
+++ app/src/main/java/com/example/template/App.kt
@@ -1,7 +1,24 @@
 package com.example.template
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.androix.startup.KoinStartup
+import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.core.logger.Level
+import org.koin.dsl.koinConfiguration
 
-@HiltAndroidApp
-class App : Application()
+@OptIn(KoinExperimentalAPI::class)
+class App : Application(), KoinStartup {
+    // Startup created to support Android Initializers (which must come BEFORE onCreate())
+    // https://insert-koin.io/docs/reference/koin-android/start/#start-koin-with-androidx-startup-401-experimental
+    override fun onKoinStartup() = koinConfiguration {
+
+        androidContext(this@App)
+        androidLogger(Level.INFO)
+
+//        modules(getAllKoinModules())
+
+//        workManagerFactory()
+    }
+}
